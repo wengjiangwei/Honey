af11cf6ce85b95557ac64ceb096f18c8dd9bfb17
weng jiangwei <wengjiangwei@tuxingkeji.com>
2022-08-12 10:50:55 +0800

    .gitingore

--- demo/train.py
+++ demo/train.py
@@ -5,10 +5,16 @@
 @Time    :   2022/08/08 14:22:30
 @Author  :   Weng Jiangwei 
 @Github :    https://github.com/wengjiangwei
-@Desc    :   None
+@Desc    :   
+# send the training report to email
+python train.py  --config_path ../demo/configs.yaml  --email_config ../demo/email_config.yaml  
+            # Not send the training report to email
+python train.py  --config_path ../demo/configs.yaml
+# or  debug mode -> 
+        import train; train.run(config_path = r'../demo/configs.yaml',email_config = r'../email_config.yaml')
 '''
 
-from __future__ import annotations
+
 import os
 import sys
 from tkinter import E
@@ -21,11 +27,10 @@ from Honey.libs.det_module.yolov5_v6 import train as yolov5_train
 from Honey.tools.ReportUtils import TrainReport_yolov5
 from Honey.utils.EmailUtils import config2email,config2email_error
 import traceback
-#TODO: YOLOV5  
+#TODO: YOLOV5  --- other model
 
 def main(opt:argparse): 
     """train the yolov5 with yaml settings file
-
     Parameters
     ----------
     opt : argparse
@@ -54,27 +59,45 @@ def main(opt:argparse):
         # send email
         if opt.email_config is not None: 
             config2email(opt.email_config,email_attachment)
+
     except:
+        # send error email
         if opt.email_config is not None: # send email
             config2email_error(opt.email_config,traceback.format_exc())
 
 def parse_opt(known=False):
-
+    # argparse in terminal
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_path', type=str, help='initial filt path')
     parser.add_argument('--email_config', type=str, default=None, help='initial filt path')
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
-def run_(**kwargs):
+def run(**kwargs):
+    # Usage: import train; train.run(config_path = r'../demo/configs.yaml',email_config = r'../email_config.yaml')
 
     opt = parse_opt(True)
     for k, v in kwargs.items():
         setattr(opt, k, v)
     main(opt)
+
     return opt
 
-def report_max_idx(project_path, name):
+def report_max_idx(project_path:str, name:str)->str:
+    """Find the latest version of the project save path
+    (According to the max index of the project)
 
+    Parameters
+    ----------
+    project_path : str
+        the  project save path
+    name : str
+        project_version
+
+    Returns
+    -------
+    str
+        the latest version of the project save path
+    """
     project_all = []
     project_list = os.listdir(project_path)
     for filename in project_list:
@@ -88,9 +111,8 @@ def report_max_idx(project_path, name):
 
 if __name__ == '__main__':
 
-    run_(config_path = r'/home/wjw/Work/Honey/demo/configs.yaml',email_config = r'/home/wjw/Work/Honey/email_config.yaml')
-    # report_max_idx('/home/wjw/Work/Runs/v5-ft','ft')
-    # email_config = r'/home/wjw/Work/Honey/email_config.yaml'
-    # email_config = yaml.load(open(email_config), Loader=yaml.FullLoader)
-    # config2email(email_config)
+    opt = parse_opt()
+    main(opt)
+
+    # run(config_path = r'../demo/configs.yaml',email_config = r'../email_config.yaml')
     ...
